e95c49f9671565abb99ed6b1de7f6c2d541d0153
Izdihar Zuhdi <izdiharzuhdi@MacBook-Pro.local>
2017-02-05 12:11:42 +0800

    First Commiy

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '374373629574497', // your App ID
         'clientSecret'  : '9006880dae7f34330e81c37078ccc226', // your App Secret
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL': 'https://momokaka.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
+        'callbackURL'       : 'https://momokaka.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : 'your-secret-clientID-here',
         'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://momokaka.herokuapp.com/auth/google/callback'
     }
 
 };
